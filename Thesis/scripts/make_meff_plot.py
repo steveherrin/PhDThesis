9cd7e5630d3812159126582a41eabc872662f5f3
Steve Herrin <steve.herrin@gmail.com>
2013-03-19 13:11:01 -0700

    Updated to do the errors properly.

--- Thesis/scripts/make_meff_plot.py
+++ Thesis/scripts/make_meff_plot.py
@@ -5,18 +5,12 @@ from math import sqrt, fabs, log10
 class Parameter:
     def __init__(self, bf, m1s, p1s, m2s, p2s, m3s, p3s):
         self.bf = bf
-        self.m1s = m1s
-        self.p1s = p1s
-        self.m2s = m2s
-        self.p2s = p2s
-        self.m3s = m3s
-        self.p3s = p3s
-
-class Result:
-    def __init__(self):
-        self.s1 = 0
-        self.s2 = 0
-        self.s3 = 0
+        self.m = [bf, m1s, m2s, m3s]
+        self.p = [bf, p1s, p2s, p3s]
+        self.CalcDeltas()
+    def CalcDeltas(self):
+        self.d_m = [self.bf - m for m in self.m]
+        self.d_p = [p - self.bf for p in self.p]
 
 # From arXiv:1205.4018
 m_sol = Parameter(7.62e-5, 7.43e-5, 7.81e-5, 7.27e-5,
@@ -31,84 +25,138 @@ s_23_ih = Parameter(0.600, 0.569, 0.626, 0.390, 0.650, 0.370, 0.670)
 s_13_nh = Parameter(0.0246, 0.0218, 0.0275, 0.019, 0.030, 0.017, 0.033)
 s_13_ih = Parameter(0.0250, 0.0223, 0.0276, 0.020, 0.030, 0.017, 0.033)
 
+# limits
+exo_limit = 0.140
+
+# 0.3 eV >> mass splittings, so expand to first order and call it a day
+cosmo_sum = 0.3
+cosmo_limit = cosmo_sum/3
 
 
 # plot range
 log_m_lo = log10(0.9e-4)
 log_m_hi = log10(1.1)
-n = 1000
+n = 2000
 
-def get_ih_hi(m):
-    result = Result()
-    result.s1 = (m*s_13_ih.m1s
-                 + sqrt(m**2 + m_atm_ih.p1s)*s_12.p1s
-                 + sqrt(m**2 + m_atm_ih.p1s - m_sol.m1s)*(1-s_12.p1s)*(1-s_13_ih.m1s))
-    result.s2 = (m*s_13_ih.m2s
-                 + sqrt(m**2 + m_atm_ih.p2s)*s_12.p2s
-                 + sqrt(m**2 + m_atm_ih.p2s - m_sol.m2s)*(1-s_12.p2s)*(1-s_13_ih.m2s))
-    result.s3 = (m*s_13_ih.m3s
-                 + sqrt(m**2 + m_atm_ih.p3s)*s_12.p3s
-                 + sqrt(m**2 + m_atm_ih.p3s - m_sol.m3s)*(1-s_12.p3s)*(1-s_13_ih.m3s))
+def m_sum2(m1, m2):
+    return sqrt(m1**2 +m2)
+
+def m_sum3(m1, m2, m3):
+    return sqrt(m1**2 + m2 + m3)
+
+def sigma_m_sol_nh(m, s_12, s_23, s_13, m_sol, m_atm, pm_1, pm_2, d_m_sol):
+    d = pm_1*(1-s_12)*(1-s_13)/(2*m_sum2(m, m_sol))
+    return d * d_m_sol
+
+def sigma_m_sol_ih(m, s_12, s_23, s_13, m_sol, m_atm, pm_1, pm_2, d_m_sol):
+    d = (1-s_12**2)*(1-s_13)/(2*m_sum3(m, m_sol, m_atm))
+    return d * d_m_sol
+
+def sigma_s_12_nh(m, s_12, s_23, s_13, m_sol, m_atm, pm_1, pm_2, d_s_12):
+    d = (-m + pm_1*m_sum2(m, m_atm))*(1-s_13)
+    return d * d_s_12
+
+def sigma_s_12_ih(m, s_12, s_23, s_13, m_sol, m_atm, pm_1, pm_2, d_s_12):
+    d = (-m_sum2(m, m_atm) + pm_1*m_sum3(m, m_atm, m_sol))*(1-s_13)
+    return d * d_s_12
+
+def sigma_m_atm_nh(m, s_12, s_23, s_13, m_sol, m_atm, pm_1, pm_2, d_m_atm):
+    d = pm_2*s_13/(2*m_sum2(m, m_atm))
+    return d * d_m_atm
+
+def sigma_m_atm_ih(m, s_12, s_23, s_13, m_sol, m_atm, pm_1, pm_2, d_m_atm):
+    d = (1-s_12**2)*(1-s_13)/(2*m_sum2(m, m_atm))
+    return d * d_m_atm
+
+def sigma_s_13_nh(m, s_12, s_23, s_13, m_sol, m_atm, pm_1, pm_2, d_s_13):
+    d = -m*(1-s_12)-pm_1*m_sum2(m, m_sol)+pm_2*m_sum2(m, m_atm)
+    return d * d_s_13
+
+def sigma_s_13_ih(m, s_12, s_23, s_13, m_sol, m_atm, pm_1, pm_2, d_s_13):
+    d = -m_sum2(m, m_atm)*(1-s_12)-pm_1*m_sum3(m, m_atm, m_sol)+pm_2*m
+    return d * d_s_13
+
+def get_ih(m, pm_1, pm_2, err_dir):
+
+    result = [0, 0, 0, 0]
+    
+    result[0] = (m_sum2(m, m_atm_ih.bf)*(1-s_12.bf)*(1-s_13_ih.bf)
+                 + pm_1 * m_sum3(m, m_atm_ih.bf, m_sol.bf)*s_12.bf*(1-s_13_ih.bf)
+                 + pm_2 * m * s_13_ih.bf)
+    if result[0] < 0:
+        err_dir *= -1
+
+    for n_sigma in xrange(1, 3+1):
+
+        sum_sq_sigma = 0
+
+        for (f, x) in [(sigma_m_sol_ih, m_sol),
+                       (sigma_m_atm_ih, m_atm_ih),
+                       (sigma_s_12_ih, s_12),
+                       (sigma_s_13_ih, s_13_ih)]:
+
+            added_sigma = 0
+            for dx in [x.d_m[n_sigma], x.d_p[n_sigma]]:
+    
+                sigma = f(m, s_12.bf, s_23_ih.bf,
+                          s_13_ih.bf, m_sol.bf,
+                          m_atm_ih.bf, pm_1, pm_2, dx)
+                
+                if err_dir > 0 and sigma > added_sigma:
+                    added_sigma = sigma
+                elif err_dir < 0 and sigma < added_sigma:
+                    added_sigma = sigma
+
+            sum_sq_sigma += added_sigma**2
+
+        if err_dir > 0:
+            result[n_sigma] = result[0] + sqrt(sum_sq_sigma)
+        else:
+            result[n_sigma] = result[0] - sqrt(sum_sq_sigma)
+      
     return result
 
-def get_ih_lo(m):
-    result = Result()
-    result.s1 = fabs(m*s_13_ih.p1s
-                 + sqrt(m**2 + m_atm_ih.m1s)*s_12.p1s
-                 - sqrt(m**2 + m_atm_ih.m1s - m_sol.p1s)*(1-s_12.p1s)*(1-s_13_ih.p1s))
-    result.s2 = fabs(m*s_13_ih.p2s
-                 + sqrt(m**2 + m_atm_ih.m2s)*s_12.p2s
-                 - sqrt(m**2 + m_atm_ih.m2s - m_sol.p2s)*(1-s_12.p2s)*(1-s_13_ih.p2s))
-    result.s3 = fabs(m*s_13_ih.p3s
-                 + sqrt(m**2 + m_atm_ih.m3s)*s_12.p3s
-                 - sqrt(m**2 + m_atm_ih.m3s - m_sol.p3s)*(1-s_12.p3s)*(1-s_13_ih.p3s))
-    return result
 
-def get_nh_hi(m):
-    result = Result()
-    result.s1 = (m*(1-s_12.p1s)*(1-s_13_nh.p1s)
-                 + sqrt(m**2 + m_sol.p1s)*s_12.p1s*(1-s_13_nh.p1s)
-                 + sqrt(m**2 + m_atm_nh.p1s)*s_13_nh.p1s)
-    result.s2 = (m*(1-s_12.p2s)*(1-s_13_nh.p2s)
-                 + sqrt(m**2 + m_sol.p2s)*s_12.p2s*(1-s_13_nh.p2s)
-                 + sqrt(m**2 + m_atm_nh.p2s)*s_13_nh.p2s)
-    result.s3 = (m*(1-s_12.p3s)*(1-s_13_nh.p3s)
-                 + sqrt(m**2 + m_sol.p3s)*s_12.p3s*(1-s_13_nh.p3s)
-                 + sqrt(m**2 + m_atm_nh.p3s)*s_13_nh.p3s)
-    return result
+def get_nh(m, pm_1, pm_2, err_dir):
 
-def get_nh_lo(m):
-    result = Result()
-    result.s1 = (m*(1-s_12.p1s)*(1-s_13_nh.p1s)
-                 - sqrt(m**2 + m_sol.p1s)*s_12.p1s*(1-s_13_nh.p1s)
-                 - sqrt(m**2 + m_atm_nh.p1s)*s_13_nh.p1s)
-    if result.s1 < 0:
-        result.s1 = -(m*(1-s_12.m1s)*(1-s_13_nh.p1s)
-                      - sqrt(m**2 + m_sol.m1s)*s_12.m1s*(1-s_13_nh.p1s)
-                      + sqrt(m**2 + m_atm_nh.p1s)*s_13_nh.p1s)
-        if result.s1 < 0:
-            result.s1 = 0
-    result.s2 = (m*(1-s_12.p2s)*(1-s_13_nh.p2s)
-                 - sqrt(m**2 + m_sol.p2s)*s_12.p2s*(1-s_13_nh.p2s)
-                 - sqrt(m**2 + m_atm_nh.p2s)*s_13_nh.p2s)
-    if result.s2 < 0:
-        result.s2 = -(m*(1-s_12.m2s)*(1-s_13_nh.p2s)
-                      - sqrt(m**2 + m_sol.m2s)*s_12.m2s*(1-s_13_nh.p2s)
-                      + sqrt(m**2 + m_atm_nh.p2s)*s_13_nh.p2s)
-        if result.s2 < 0:
-            result.s2 = 0
-    result.s3 = (m*(1-s_12.p3s)*(1-s_13_nh.p3s)
-                 - sqrt(m**2 + m_sol.p3s)*s_12.p3s*(1-s_13_nh.p3s)
-                 - sqrt(m**2 + m_atm_nh.p3s)*s_13_nh.p3s)
-    if result.s3 < 0:
-        result.s3 = -(m*(1-s_12.m3s)*(1-s_13_nh.p3s)
-                      - sqrt(m**2 + m_sol.m3s)*s_12.m3s*(1-s_13_nh.p3s)
-                      + sqrt(m**2 + m_atm_nh.p3s)*s_13_nh.p3s)
-        if result.s3 < 0:
-            result.s3 = 0
+    result = [0, 0, 0, 0]
+    
+    result[0] = (m*(1-s_12.bf)*(1-s_13_nh.bf)
+                 + pm_1 * m_sum2(m, m_sol.bf)*s_12.bf*(1-s_13_nh.bf)
+                 + pm_2 * m_sum2(m, m_atm_nh.bf) * s_13_nh.bf)
+
+
+    for n_sigma in xrange(1, 3+1):
+
+        sum_sq_sigma = 0
+
+        for (f, x) in [(sigma_m_sol_nh, m_sol),
+                       (sigma_m_atm_nh, m_atm_nh),
+                       (sigma_s_12_nh, s_12),
+                       (sigma_s_13_nh, s_13_nh)]:
+
+            added_sigma = 0
+            for dx in [x.d_m[n_sigma], x.d_p[n_sigma]]:
+    
+                sigma = f(m, s_12.bf, s_23_nh.bf,
+                          s_13_nh.bf, m_sol.bf,
+                          m_atm_nh.bf, pm_1, pm_2, dx)
+
+                if err_dir > 0 and sigma > added_sigma:
+                    added_sigma = sigma
+                elif err_dir < 0 and sigma < added_sigma:
+                    added_sigma = sigma
+            sum_sq_sigma += added_sigma**2
+        if err_dir > 0:
+            result[n_sigma] = result[0] + sqrt(sum_sq_sigma)
+        else:
+            result[n_sigma] = result[0] - sqrt(sum_sq_sigma)
+      
     return result
 
 m_l = []
+
+
 
 nh_lo_l = []
 nh_hi_l = []
@@ -117,16 +165,47 @@ ih_lo_l = []
 ih_hi_l = []
 
 for i, m in enumerate(numpy.logspace(log_m_lo, log_m_hi, n, False)):
-    nh_lo = get_nh_lo(m)
-    nh_hi = get_nh_hi(m)
-    ih_lo = get_ih_lo(m)
-    ih_hi = get_ih_hi(m)
+    nh_lo_mm = get_nh(m, -1, -1, -1)
+    # yes, positive error on this one because this curve only
+    # shows up when the expression is negative
+    nh_lo_mp = get_nh(m, -1, 1, 1)
+
+    nh_lo = []
+    for (y1, y2) in zip(nh_lo_mm, nh_lo_mp):
+        if y1 > 0:
+            nh_lo.append(y1)
+        elif y1 < 0 and y2 < 0:
+            # colvoluted, and there's probably a better way
+            nh_lo.append(fabs(nh_lo_mp[0])-fabs(y2-nh_lo_mp[0]))
+        else:
+            nh_lo.append(0)
+    
+    nh_hi_pp = get_nh(m, 1, 1, 1)
+    nh_hi_pm = get_nh(m, 1, -1, 1)
+    nh_hi = [max(y1, y2) for (y1, y2) in zip(nh_hi_pp, nh_hi_pm)]
+    
+    ih_lo_mm = get_ih(m, -1, -1, -1)
+    ih_lo_mp = get_ih(m, -1, 1, -1)
+    ih_lo = [min(y1, y2) for (y1, y2) in zip(ih_lo_mm, ih_lo_mp)]
+    ih_hi_pp = get_ih(m, 1, 1, 1)
+    ih_hi_pm = get_ih(m, 1, -1, 1)
+    ih_hi = [max(y1, y2) for (y1, y2) in zip(ih_hi_pp, ih_hi_pm)]
 
     m_l.append(m)
     nh_lo_l.append(nh_lo)
     nh_hi_l.append(nh_hi)
     ih_lo_l.append(ih_lo)
     ih_hi_l.append(ih_hi)
+
+print("For a limit on m_bb of %0.3e eV:"%(exo_limit))
+found_limit = [False for x in xrange(4)]
+for (m, y) in zip(m_l, ih_lo_l):
+    for i in xrange(1, len(found_limit)):
+        if (not found_limit[i]) and y[i] > exo_limit:
+            print("    %i sigma limit on m_min: %0.3e eV"%(i, m))
+            found_limit[i] = True
+            
+    
     
 g_nh_1s = ROOT.TGraph(2*n)
 g_nh_2s = ROOT.TGraph(2*n)
@@ -136,21 +215,21 @@ g_ih_2s = ROOT.TGraph(2*n)
 g_ih_3s = ROOT.TGraph(2*n)
 
 for i in range(n):
-    g_nh_1s.SetPoint(i, m_l[i], nh_hi_l[i].s1)
-    g_nh_1s.SetPoint(n+i, m_l[n-i-1], nh_lo_l[n-i-1].s1)
+    g_nh_1s.SetPoint(i, m_l[i], nh_hi_l[i][1])
+    g_nh_1s.SetPoint(n+i, m_l[n-i-1], nh_lo_l[n-i-1][1])
     #g_nh_1s.SetPoint(n+i, m_l[n-i-1], 0)
-    g_nh_2s.SetPoint(i, m_l[i], nh_hi_l[i].s2)
-    g_nh_2s.SetPoint(n+i, m_l[n-i-1], nh_lo_l[n-i-1].s2)
+    g_nh_2s.SetPoint(i, m_l[i], nh_hi_l[i][2])
+    g_nh_2s.SetPoint(n+i, m_l[n-i-1], nh_lo_l[n-i-1][2])
     #g_nh_2s.SetPoint(n+i, m_l[n-i-1], 0)
-    g_nh_3s.SetPoint(i, m_l[i], nh_hi_l[i].s3)
-    g_nh_3s.SetPoint(n+i, m_l[n-i-1], nh_lo_l[n-i-1].s3)
+    g_nh_3s.SetPoint(i, m_l[i], nh_hi_l[i][3])
+    g_nh_3s.SetPoint(n+i, m_l[n-i-1], nh_lo_l[n-i-1][3])
     #g_nh_3s.SetPoint(n+i, m_l[n-i-1], 0)
-    g_ih_1s.SetPoint(i, m_l[i], ih_hi_l[i].s1)
-    g_ih_1s.SetPoint(n+i, m_l[n-i-1], ih_lo_l[n-i-1].s1)
-    g_ih_2s.SetPoint(i, m_l[i], ih_hi_l[i].s2)
-    g_ih_2s.SetPoint(n+i, m_l[n-i-1], ih_lo_l[n-i-1].s2)
-    g_ih_3s.SetPoint(i, m_l[i], ih_hi_l[i].s3)
-    g_ih_3s.SetPoint(n+i, m_l[n-i-1], ih_lo_l[n-i-1].s3)
+    g_ih_1s.SetPoint(i, m_l[i], ih_hi_l[i][1])
+    g_ih_1s.SetPoint(n+i, m_l[n-i-1], ih_lo_l[n-i-1][1])
+    g_ih_2s.SetPoint(i, m_l[i], ih_hi_l[i][2])
+    g_ih_2s.SetPoint(n+i, m_l[n-i-1], ih_lo_l[n-i-1][2])
+    g_ih_3s.SetPoint(i, m_l[i], ih_hi_l[i][3])
+    g_ih_3s.SetPoint(n+i, m_l[n-i-1], ih_lo_l[n-i-1][3])
 
 g_nh_1s.SetFillStyle(1001)
 g_nh_1s.SetFillColor(ROOT.kBlue+2)
@@ -158,7 +237,6 @@ g_nh_2s.SetFillStyle(1001)
 g_nh_2s.SetFillColor(ROOT.kBlue+1)
 g_nh_3s.SetFillStyle(1001)
 g_nh_3s.SetFillColor(ROOT.kBlue+0)
-
 g_ih_1s.SetFillStyle(1001)
 g_ih_1s.SetFillColor(ROOT.kBlue+2)
 g_ih_2s.SetFillStyle(1001)
@@ -175,7 +253,7 @@ g_ih_3s.Draw("F")
 g_ih_2s.Draw("F")
 g_ih_1s.Draw("F")
 
-l = ROOT.TLegend(.60, .15, .75, .30)
+l = ROOT.TLegend(.775, .15, .875, .30)
 l.SetFillColor(ROOT.kWhite)
 l.AddEntry(g_nh_1s, "1 #sigma", "f")
 l.AddEntry(g_nh_2s, "2 #sigma", "f")
@@ -214,25 +292,26 @@ t_ih.SetTextFont(42)
 t_ih.Draw()
 
 g_exo = ROOT.TGraph(2)
-g_exo.SetPoint(0, graph_lo, .140)
-g_exo.SetPoint(1, graph_hi, .140)
+g_exo.SetPoint(0, graph_lo, exo_limit)
+g_exo.SetPoint(1, graph_hi, exo_limit)
 g_exo.SetLineColor(ROOT.kRed)
 g_exo.SetLineStyle(2)
 g_exo.Draw()
-t_exo = ROOT.TText(8e-4, .16, "EXO-200 Limit")
+t_exo = ROOT.TText(6e-4, .16, "EXO-200 Limit")
 t_exo.SetTextColor(ROOT.kRed)
 t_exo.SetTextSize(0.05)
 t_exo.SetTextFont(42)
 t_exo.Draw()
 
+#print("Cosmo limit was %f."%(cosmo_limit))
 
 g_cosmo = ROOT.TGraph(2)
-g_cosmo.SetPoint(0, 0.3, graph_lo)
-g_cosmo.SetPoint(1, 0.3, graph_hi)
+g_cosmo.SetPoint(0, cosmo_limit, graph_lo)
+g_cosmo.SetPoint(1, cosmo_limit, graph_hi)
 g_cosmo.SetLineColor(ROOT.kRed)
 g_cosmo.SetLineStyle(2)
 g_cosmo.Draw()
-t_cosmo = ROOT.TText(.35, 3e-2, "Cosmological Limit")
+t_cosmo = ROOT.TText(cosmo_limit+0.02, 2e-2, "Cosmological Limit")
 t_cosmo.SetTextColor(ROOT.kRed)
 t_cosmo.SetTextSize(0.05)
 t_cosmo.SetTextFont(42)
