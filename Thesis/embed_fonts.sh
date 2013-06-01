3c3d86aa2e0feb1adf4afe5690de1eeecaf7d252
Steve Herrin <steve.herrin@gmail.com>
2013-05-31 20:00:10 -0700

    Stop it from auto-rotating my pages.

--- Thesis/embed_fonts.sh
+++ Thesis/embed_fonts.sh
@@ -3,4 +3,4 @@
 INFILE=$1
 OUTFILE="${INFILE%.*}_embedded.pdf"
 
-gs -dNOPAUSE -dBATCH -dEmbedAllFonts=true -sDEVICE=pdfwrite -o ${OUTFILE} -c ".setpdfwrite <</NeverEmbed [ ]>> setdistillerparams" -f $INFILE
+gs -dNOPAUSE -dBATCH -dAutoRotatePages=/None -dEmbedAllFonts=true -sDEVICE=pdfwrite -o ${OUTFILE} -c ".setpdfwrite <</NeverEmbed [ ]>> setdistillerparams" -f $INFILE
