0272bf94d5ef0ec988d19da574a1994ba24db5ab
Steve Herrin <steve.herrin@gmail.com>
2013-06-02 18:14:59 -0700

    Now the script embeds author and title info, as well.

--- Thesis/embed_fonts.sh
+++ Thesis/embed_fonts.sh
@@ -3,4 +3,11 @@
 INFILE=$1
 OUTFILE="${INFILE%.*}_embedded.pdf"
 
+AUTHOR="Steven Herrin"
+TITLE="Double beta decay in xenon-136"
+
 gs -dNOPAUSE -dBATCH -dAutoRotatePages=/None -dEmbedAllFonts=true -sDEVICE=pdfwrite -o ${OUTFILE} -c ".setpdfwrite <</NeverEmbed [ ]>> setdistillerparams" -f $INFILE
+exiftool -overwrite_original -Title="${TITLE}" -Author="${AUTHOR}" -Creator="" ${OUTFILE}
+
+echo "Author set to: ${AUTHOR}"
+echo "Title  set to: ${TITLE}"
