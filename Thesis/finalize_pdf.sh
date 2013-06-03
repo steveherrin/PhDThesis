1c010d734e4043b9d95e79278e2fc6394be352c0
Steve Herrin <steve.herrin@gmail.com>
2013-06-02 18:23:48 -0700

    Just commenting a little.

--- Thesis/finalize_pdf.sh
+++ Thesis/finalize_pdf.sh
@@ -6,8 +6,11 @@ OUTFILE="${INFILE%.*}_embedded.pdf"
 AUTHOR="Steven Herrin"
 TITLE="Double beta decay in xenon-136"
 
+# Embed all the fonts, even the universal ones (with the .setpdfwrite part)
 gs -dNOPAUSE -dBATCH -dAutoRotatePages=/None -dEmbedAllFonts=true -sDEVICE=pdfwrite -o ${OUTFILE} -c ".setpdfwrite <</NeverEmbed [ ]>> setdistillerparams" -f $INFILE
+# Add author and title information, and strip out creator information
 exiftool -overwrite_original -Title="${TITLE}" -Author="${AUTHOR}" -Creator="" ${OUTFILE}
 
 echo "Author set to: ${AUTHOR}"
 echo "Title  set to: ${TITLE}"
+echo "Final PDF written to: ${OUTFILE}"
